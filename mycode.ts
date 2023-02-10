5c4590afe23f2ee225d7100a8a455a8bf9f0ae0f
Bradley <gauthieb@mail.gvsu.edu>
2023-02-10 16:11:02 -0500

    created Course type and Course array with classes in

--- mycode.ts
+++ mycode.ts
@@ -1,1 +1,24 @@
-console.log("Hello World")
+type Course = {
+    courseNumber: string;
+    courseName: string;
+    semesterTaken: string;
+}
+
+let course1 = {
+    courseNumber: "371",
+    courseName: "Web Application Programming",
+    semesterTaken: "Winter"
+}
+let course2 = {
+    courseNumber: "360",
+    courseName: "Information Management and Science",
+    semesterTaken: "Winter"
+}
+let course3 = {
+    courseNumber: "320",
+    courseName: "Visualization of Data and Information",
+    semesterTaken: "Winter"
+}
+
+const myCourses: Array<Course> = [course1, course2, course3];
+
